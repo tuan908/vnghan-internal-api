025e1802897fd2e10f2a155d51a86a05cf50f04f
tuanna <tuan.a1.bt@gmail.com>
2025-03-21 00:02:56 +0700

    no need to paging now

--- src/routes/screw.ts
+++ src/routes/screw.ts
@@ -2,8 +2,7 @@ import {
   DEFAULT_MATERIAL_ID,
   DEFAULT_SIZE_ID,
   DEFAULT_TYPE_ID,
-  ErrorCodes,
-  PAGE_SIZE,
+  ErrorCodes
 } from "@/constants";
 import json from "@/i18n/locales/vi.json";
 import { createErrorResponse, createSuccessResponse } from "@/lib/api-response";
@@ -11,7 +10,7 @@ import SCHEMA from "@/lib/db";
 import { nullsToUndefined } from "@/lib/utils";
 import { ScrewDto } from "@/lib/validations";
 import { ScrewTypeDto, ServerCreateScrewDto } from "@/types";
-import { desc, eq, sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
 const screwRouter = new Hono();
@@ -19,16 +18,16 @@ const screwRouter = new Hono();
 screwRouter
   .get("/", async (c) => {
     const db = c.get("db");
-    const { page = "0" } = c.req.query();
-    const pageNumber = parseInt(page, 10) || 0;
+    // const { page = "0" } = c.req.query();
+    // const pageNumber = parseInt(page, 10) || 0;
 
-    const totalCountResult = await db
-      .select({ count: sql`count(*)`.mapWith(Number) })
-      .from(SCHEMA.SCREW)
-      .where(eq(SCHEMA.SCREW.isDeleted, false));
+    // const totalCountResult = await db
+    //   .select({ count: sql`count(*)`.mapWith(Number) })
+    //   .from(SCHEMA.SCREW)
+    //   .where(eq(SCHEMA.SCREW.isDeleted, false));
 
-    const totalCount = totalCountResult[0]?.count || 0;
-    const totalPages = Math.ceil(totalCount / PAGE_SIZE);
+    // const totalCount = totalCountResult[0]?.count || 0;
+    // const totalPages = Math.ceil(totalCount / PAGE_SIZE);
 
     const screws = await db
       .select({
@@ -51,23 +50,25 @@ screwRouter
         eq(SCHEMA.SCREW.componentTypeId, SCHEMA.SCREW_TYPE.id)
       )
       .where(eq(SCHEMA.SCREW.isDeleted, false))
-      .orderBy(desc(SCHEMA.SCREW.id))
-      .limit(PAGE_SIZE)
-      .offset(pageNumber * PAGE_SIZE);
+      .orderBy(SCHEMA.SCREW.id);
+    // .limit(PAGE_SIZE)
+    // .offset(pageNumber * PAGE_SIZE);
 
-    return c.json(
-      createSuccessResponse(nullsToUndefined(screws), {
-        pagination: {
-          page: pageNumber,
-          totalPages,
-          totalItems: totalCount,
-          pageSize: PAGE_SIZE,
-          hasNextPage: pageNumber < totalPages - 1,
-          hasPreviousPage: pageNumber > 0,
-        },
-      }),
-      200
-    );
+    // return c.json(
+    //   createSuccessResponse(nullsToUndefined(screws), {
+    //     pagination: {
+    //       page: pageNumber,
+    //       totalPages,
+    //       totalItems: totalCount,
+    //       pageSize: PAGE_SIZE,
+    //       hasNextPage: pageNumber < totalPages - 1,
+    //       hasPreviousPage: pageNumber > 0,
+    //     },
+    //   }),
+    //   200
+    // );
+
+    return c.json(createSuccessResponse(nullsToUndefined(screws)), 200);
   })
   .post("/", async (c) => {
     const db = c.get("db");
